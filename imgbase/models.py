d511f3ae7ae271ec88ea390e06cf2fb0a3533d65
Austin Pocus <ajpocus@gmail.com>
2013-12-31 04:45:03 -0500

    Dem VersionErrors...

--- imgbase/models.py
+++ imgbase/models.py
@@ -1,2 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.ForeignKey(User)
+    oauth_token = models.CharField(max_length=255)
+    oauth_secret = models.CharField(max_length=255)
 
--- imgbase/views.py
+++ imgbase/views.py
@@ -1,20 +1,34 @@
+import os
+import cgi
+
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+import oauth2
 
 from .auth import ImgurAuth
+
+consumer = oauth2.Consumer(os.environ['IMGUR_CLIENT_ID'],
+    os.environ['IMGUR_CLIENT_SECRET'])
+client = oauth2.Client(consumer)
+base_url = "https://api.imgur.com"
+authorize_url = base_url + "/oauth2/authorize"
+token_url = base_url + "/oauth2/token"
+redirect_url = "http://localhost:8000/auth/granted"
 
 def home(request):
     print(ImgurAuth.access_token)
     render(request, 'home.html')
 
 def auth(request):
-    redirect_uri = 'http://localhost:8000/auth/granted'
-    params = {
-        'scope': '',
-        'response_type': 'code',
-        'redirect_uri': redirect_uri
-    }
-    
-    url = ImgurAuth.get_authorize_url(**params)
+    resp, content = client.request(authorize_url, "GET")
+    if resp['status'] != '200':
+        raise Exception("Invalid response from Twitter.")
+
+    request.session['request_token'] = dict(cgi.parse_qsl(content))
+    url = "%s?oauth_token=%s" % (token_url,
+        request.session['request_token']['oauth_token'])
     return redirect(url)
     
 def granted(request):
